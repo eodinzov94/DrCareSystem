6e21ca52f6ccd9f498511a007e44004ef5d7fbf3
Ron Vayner <ronv.sce@gmail.com>
2021-05-12 17:07:41 +0300

    new patient, groundwork for new visit
    added 'new patient' screen logic, TODO in code, added ground work for 'new visit' logic

--- DrCareSystem/patient/urls.py
+++ DrCareSystem/patient/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("patients", views.patients, name='patients'),
     path("getVisits", views.getVisits, name='getVisits'),
     path("getInfo", views.getInfo, name='getInfo'),
+    path("newPatient",views.newPatient,name='newPatient'),
+    path("newVisit",views.newVisit,name='newVisit')
 ]
--- DrCareSystem/patient/views.py
+++ DrCareSystem/patient/views.py
@@ -19,6 +19,14 @@ def getVisits (request):
         return redirect('patients')
 
 @login_required
+def newVisit (request): #TODO: Add functionality
+    if request.method == 'POST':
+        id      = request.POST['id']
+        patient = Patient.objects.get(id=id)
+        return render(request, 'newVisit.html', {'patient':patient})
+        
+
+@login_required
 def getInfo(request):
     if request.method == 'POST':
         id      = request.POST['id']
@@ -28,3 +36,39 @@ def getInfo(request):
         return render(request, 'medinfo.html', {'results': results,'quest': quest})
     else:
         return redirect('patients')
+
+
+@login_required
+def newPatient(request): #TODO: make error appear on screen.
+    if request.method == 'POST':
+        fullName = request.POST.get('fullname')
+        personID = request.POST.get('personid')
+        tel = request.POST.get('tel')
+        gender = request.POST.get('gender')
+        age = request.POST.get('age')
+
+        if len(fullName) < 4:
+            messages.info(request, 'Name too short!')
+            return redirect('newPatient')
+        if len(personID) != 9:
+            messages.info(request, 'ID Must be 9 digits long!')
+            return redirect('newPatient')
+        if len(tel) != 10:
+            messages.info(request, 'Phone must be 10 digits long!')
+            return redirect('newPatient')
+        if age < 0 or age > 140:
+            messages.info(request, 'Illegal age!')
+            return redirect('newPatient')        
+
+        newP = Patient(
+            fullName=fullName,
+            personID=personID,
+            tel=tel,
+            gender=gender,
+            age=age,
+        )
+
+        newP.save()
+        return redirect('patients')
+    else:
+        return render(request, 'newPatient.html')
